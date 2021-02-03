989e692a778fc3dc39c5fd53338a1c2c08cf207f
Greg Lund <greg.lund@colorado.edu>
2021-02-03 05:35:52 +0000

    Added gpio functionality

--- em-cubesat-control/cubesat_client.py
+++ em-cubesat-control/cubesat_client.py
@@ -1,23 +1,38 @@
-import socket,select,pickle
+import socket,select,pickle,os
 from utils import *
+import RPi.GPIO as GPIO
 
 class CubeSatClient:
-    def __init__(self,name,hostname,port=10000,debug=True):
+    def __init__(self,master_hostname,port=10000,pwm_frequency=500,debug=True):
         '''
         CubesatClient allows for communication with the master control and for actuation on the rpi zero
 
-        name: string name of this unit
-        host: hostname for master (ip address)
+        master_hostname: hostname for master (or ip address)
         port: port to connect to (set to same as master)
         '''
 
-        self.name = name
-        self.hostname = hostname
+        # Socket connection parameters
+        self.name = os.getenv('USER')
+        self.master_hostname = master_hostname
         self.port = port
 
         # Disable printing to stdout if debug is off
         if not debug:
             sys.stdout = open(os.devnull, 'w')
+
+        # Setup GPIO
+        self.em_pins = [(26,19),(13,6)]
+
+        GPIO.setmode(GPIO.BCM)
+        for pins in self.em_pins:
+            GPIO.setup(pins[0],GPIO.OUT)
+            GPIO.setup(pins[1],GPIO.OUT)
+            GPIO.output(pins[0],0)
+            GPIO.output(pins[1],0)
+
+        # PWM frequency
+        self.pwm_frequency = pwm_frequency
+
 
     def connect_to_master(self):
         '''
@@ -25,8 +40,8 @@ class CubeSatClient:
         '''
         # Master expects a connection followed by a message containing this unit's name
         self.sckt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        print('Attempting to connect to master at %s'%self.hostname)
-        self.sckt.connect((self.hostname,self.port))
+        print('Attempting to connect to master at %s'%self.master_hostname)
+        self.sckt.connect((self.master_hostname,self.port))
         self.sckt.sendall(self.name.encode())
         self.sckt.setblocking(False)
         print('Connected!')
@@ -47,13 +62,32 @@ class CubeSatClient:
                 else:
                     print('Recieved a message from master!')
                     msg = pickle.loads(msg)
-                    print('Message type:',msg.msg_type)
+                    self.act_msg(msg)
 
+
+    def act_msg(self,msg):
+        if msg is None:
+            print("Message is None!")
+            return
+        if msg.msg_type == 'echo':
+            print('Message is echo, echoing back to master...')
+            self.sckt.sendall(msg.data.encode())
+        elif msg.msg_type == 'gpio_pwm':
+            pin = msg.data[0]
+            intensity = msg.data[1]
+            print('Message is gpio_pwm. Starting pwm on gpio %d at %f%% intensity.'%(pin,100*intensity))
+            p = GPIO.PWM(pin,self.pwm_frequency)
+            p.start(intensity*100)
+        elif msg.msg_type == 'power_em':
+            print('Message is power_em')
+            em_idx = msg.data[0]
+            intensity = msg.data[1]
 
     def __del__(self):
         self.sckt.close()
+        GPIO.cleanup()
 
 if __name__ == '__main__':
-    c = CubeSatClient('test','192.168.0.13')
+    c = CubeSatClient(master_hostname='192.168.0.13')
     c.connect_to_master()
     c.run()
--- em-cubesat-control/utils.py
+++ em-cubesat-control/utils.py
@@ -3,7 +3,27 @@ class Msg:
     Send this message from master to client to control EM output, tests etc
     '''
     def __init__(self,msg_type='echo',data=[]):
-        msg_types = ['echo','power_em','read_sensor','run_rotation','run_full_test']
+        '''
+        ---------------------------------------------
+        Overview of message types:
+        ---------------------------------------------
+        echo: a message used to test socket connection
+            - data should be of type string
+            - client should simply echo data back to master
+        gpio_pwm: a message used to test pwm on a specific gpio pin (BCM-based indexing)
+            - data should be a tupe (or list): (gpio_pin,intensity)
+                - gpio_pin should be a non-reserved, available pin and intensity float in range [0,1]
+        power_em: a message to power a specific em with a specific intensity
+            - data should be a tuple (or list): (em_id,intensity)
+                - em_id int in range [0,3], intensity float in range [-1,1]
+        read_sensor: a message to send sensor reading back to master
+            - TODO
+        run_rotation: a message to initiate a single rotation
+            - TODO
+        run_full_test: a message to initiate a full scale test
+            - TODO
+        '''
+        msg_types = ['echo','gpio_pwm','power_em','read_sensor','run_rotation','run_full_test']
         if msg_type not in msg_types:
             raise Exception("Undefined msg_type. Must be one of: %s"%msg_types)
 
